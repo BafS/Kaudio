1aab4f91d0e992e6f511b799f04f1dd908a01c4a
BafS <fabacrans@gmail.com>
2016-10-06 16:28:39 +0200

    Add mongoose, clean code

--- app.js
+++ app.js
@@ -1,25 +1,33 @@
 const express = require('express')
 const path = require('path')
 const logger = require('morgan')
-const cookieParser = require('cookie-parser')
+// const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
+const fs = require('fs')
 
-const routes = require('./routes/index')
-const users = require('./routes/users')
+const modelsPath = path.join(__dirname, 'app/models')
+
+// Bootstrap models
+fs.readdirSync(modelsPath)
+  .filter(file => ~file.search(/^[^\.].*\.js$/))
+  .forEach(file => require(path.join(modelsPath, file)))
+
+// Register routes
+const routes = require('./app/routes/index')
+const users = require('./app/routes/users')
 
 const app = express()
 
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'))
-// app.set('view engine', 'jade')
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/kaudio1')
 
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
+// app.use(cookieParser())
+// app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', routes)
 app.use('/users', users)
---
+++ app/models/user.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+  login: String,
+  email: String,
+  password: String
+})
+
+mongoose.model('User', userSchema)
