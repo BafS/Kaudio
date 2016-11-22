791709e1da4bec6a74dff9b1a0de874eb7640577
BafS <fabacrans@gmail.com>
2016-11-22 23:53:23 +0100

    Search is now case insensitive

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -23,7 +23,7 @@ exports.searchRegex = function () {
     const query = hook.params.query
     for (let field in query) {
       if (query[field].$search && field.indexOf('$') === -1) {
-        query[field] = { $regex: new RegExp(query[field].$search) }
+        query[field] = { $regex: new RegExp(query[field].$search, 'i') }
       }
     }
   }
