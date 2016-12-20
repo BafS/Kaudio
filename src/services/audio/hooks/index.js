1102b37f10ade2a1d3827b7f8751ab2f6dee42ca
Moodah <christopher.browne@heig-vd.ch>
2016-12-20 23:39:46 +0100

    Add authorization passin via path

--- src/services/audio/hooks/index.js
+++ src/services/audio/hooks/index.js
@@ -1,11 +1,24 @@
 'use strict'
 
 const globalHooks = require('../../../hooks')
-const hooks = require('feathers-hooks')
+const hooks = require('feathers-hooks-common')
 const auth = require('feathers-authentication').hooks
+
+const addTokenToHeaders = function (options) {
+  return function (hook) {
+    if (hook.params.query.authorization !== undefined) {
+      hook.params.token = hook.params.query.authorization
+    }
+
+    if (hook.params.query.Authorization !== undefined) {
+      hook.params.token = hook.params.query.Authorization
+    }
+  }
+}
 
 exports.before = {
   all: [
+    addTokenToHeaders(),
     auth.verifyToken(),
     auth.populateUser(),
     auth.restrictToAuthenticated()
